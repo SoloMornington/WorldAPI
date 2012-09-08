fada5b0adadd9011f138ebcacd2250d45d988083
SoloMornington <solomornington@gmail.com>
2012-09-07 19:55:31 -0700

    better index.php examples. :-)

--- index.php
+++ index.php
@@ -6,16 +6,16 @@ require_once('WorldAPIPlace.inc');
 require_once('WorldAPIResident.inc');
 require_once('WorldAPIGroup.inc');
 
-$resident = new WorldAPIResident('24e6998d-7bf2-4d03-b38b-acf8f2a21fc1');
-$place = new WorldAPIPlace('49ef8b3d-a0cd-8f6f-b226-b19c566d9b60');
-$wapi = new WorldAPIGroup('e0ed0f49-0de4-37c5-e7be-5f1b60de9bcf');
+$resident = new WorldAPIResident('6d286553-59ae-409a-887d-ee75df67b834');
+$place = new WorldAPIPlace('59760961-da48-f915-df85-9eff42226ba3');
+$wapi = new WorldAPIGroup('07e23275-7192-e30d-eec1-27c181e1910d');
 
 echo '<h2>Resident</h2>';
 
 $metas = $resident->worldAPI();
 
 foreach ($metas as $key => $data) {
-  echo 'place: ' . $key . ' = ' . $data . '<br/><br/>';
+  echo 'resident: ' . $key . ' = ' . $data . '<br/><br/>';
 }
 
 echo '<h2>Place</h2>';
