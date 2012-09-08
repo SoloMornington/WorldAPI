c46cf8b84a41bbe063e6c3f8d460aac572e441e9
SoloMornington <solomornington@gmail.com>
2012-09-07 19:58:21 -0700

    groups work now.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once('WorldAPIGroup.inc');
 
 $resident = new WorldAPIResident('6d286553-59ae-409a-887d-ee75df67b834');
 $place = new WorldAPIPlace('59760961-da48-f915-df85-9eff42226ba3');
-$wapi = new WorldAPIGroup('07e23275-7192-e30d-eec1-27c181e1910d');
+$group = new WorldAPIGroup('07e23275-7192-e30d-eec1-27c181e1910d');
 
 echo '<h2>Resident</h2>';
 
@@ -28,7 +28,7 @@ foreach ($metas as $key => $data) {
 
 echo '<h2>Group</h2>';
 
-$metas = $group->owner();
+$metas = $group->worldAPI();
 
 foreach ($metas as $key => $data) {
   echo 'place: ' . $key . ' = ' . $data . '<br/><br/>';
