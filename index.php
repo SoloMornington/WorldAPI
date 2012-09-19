b30c8b2ba524eedb8b418b76cb1e1f29a6a4413a
SoloMornington <solomornington@gmail.com>
2012-09-19 09:47:53 -0700

    added group back to index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ require_once('WorldAPIResident.inc');
 require_once('WorldAPIGroup.inc');
 
 $resident = new WorldAPIResident('6d286553-59ae-409a-887d-ee75df67b834');
-$place = new WorldAPIPlace('d92a6e35-396d-3de4-be08-452f3eeb2050'); // combat sim, resident owned
-//$place = new WorldAPIPlace('49ef8b3d-a0cd-8f6f-b226-b19c566d9b60'); // worldapi test, group owned
+//$place = new WorldAPIPlace('d92a6e35-396d-3de4-be08-452f3eeb2050'); // combat sim, resident owned
+$place = new WorldAPIPlace('55f36461-9015-33cb-fca0-15d49b903345'); // NCI, group owned
 $group = new WorldAPIGroup('07e23275-7192-e30d-eec1-27c181e1910d');
 
 echo '<h2>Resident</h2>';
@@ -53,15 +53,15 @@ if ($owner) {
   }
 }
 
-
-/*
 echo '<h2>Group</h2>';
 
 $metas = $group->worldAPI();
 
+echo '<img src="' . $group->imageURL() . '"></br>';
+
 foreach ($metas as $key => $data) {
   echo 'group: ' . $key . ' = ' . $data . '<br/><br/>';
-}*/
+}
 
 ?>
 </body></html>
