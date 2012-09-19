20ff4c1a4124b08d12148dcb9e1499a1682a709f
SoloMornington <solomornington@gmail.com>
2012-09-18 19:46:28 -0700

    slurl() and tighter code.

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ require_once('WorldAPIResident.inc');
 require_once('WorldAPIGroup.inc');
 
 $resident = new WorldAPIResident('6d286553-59ae-409a-887d-ee75df67b834');
-$place = new WorldAPIPlace('59760961-da48-f915-df85-9eff42226ba3');
+$place = new WorldAPIPlace('d92a6e35-396d-3de4-be08-452f3eeb2050'); // combat sim, resident owned
+//$place = new WorldAPIPlace('49ef8b3d-a0cd-8f6f-b226-b19c566d9b60'); // worldapi test, group owned
 $group = new WorldAPIGroup('07e23275-7192-e30d-eec1-27c181e1910d');
 
 echo '<h2>Resident</h2>';
@@ -27,7 +28,11 @@ echo '<h2>Place</h2>';
 
 $metas = $place->worldAPI();
 
-echo '<img src="' . $place->imageURL() . '"></br>';
+echo '<div><img src="' . $place->imageURL() . '"></div>';
+
+$parcelname = 'SLURL';
+if (isset($metas['parcel'])) $parcelname = $metas['parcel'];
+echo '<div><a href="' . $place->slurl() . '">' . $parcelname . '</a></div>';
 
 foreach ($metas as $key => $data) {
   echo 'place: ' . $key . ' = ' . $data . '<br/><br/>';
@@ -37,12 +42,15 @@ echo '<h2>Owner</h2>';
 
 $owner = $place->owner();
 
-$metas = $owner->worldAPI();
+if ($owner) {
 
-echo '<img src="' . $owner->imageURL() . '"></br>';
+  $metas = $owner->worldAPI();
 
-foreach ($metas as $key => $data) {
-  echo 'owner: ' . $key . ' = ' . $data . '<br/><br/>';
+  echo '<img src="' . $owner->imageURL() . '"></br>';
+
+  foreach ($metas as $key => $data) {
+    echo 'owner: ' . $key . ' = ' . $data . '<br/><br/>';
+  }
 }
 
 
